264b87a0d8969617cf6fed75f3053c421efd7479
mazhenyu997 <mazhenyu997@foxmail.com>
2018-05-09 17:57:06 +0800

    1.new_app

--- restful_api_2/settings.py
+++ restful_api_2/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'restful_api_2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restful_api_2',
+        'USER': 'root',
+        'PASSWORD': 'mysqlmmm',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+        'AUTOCOMMIT': True,
     }
 }
 
